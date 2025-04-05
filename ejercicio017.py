5453f383c1898960c983526d5a93b52d16fa167c
Ser2k2 <bell20002k@gmail.com>
2025-04-04 20:24:08 -0600

    New Files Created

---
+++ ejercicio017.py
@@ -0,0 +1,6 @@
+#Crea un código que calcule e imprima el promedio ponderado de los números 5, 12, 20 y 15 
+# con pesos respectivamente iguales a 1, 2, 3 y 4.
+
+promedioPonderado = (5 * 1 + 12 * 2 + 20 * 3 + 15 * 4) / (1 + 2 + 3 + 4)
+
+print(f'El promedio ponderado es: {promedioPonderado}.')
