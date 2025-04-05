a276a54993398e83a3678711f644d441bcf44745
Ser2k2 <bell20002k@gmail.com>
2025-04-05 12:05:10 -0600

    Update File

--- ejercicio006.py
+++ ejercicio006.py
@@ -2,6 +2,6 @@
 # y luego imprima "Hola, [nombre], tienes [edad] años."
 
 nombre = input('Escriba su nombre: ')
-edad = input('Escriba su edad: ')
+edad = int(input('Escriba su edad: '))
 
 print(f'Hola, {nombre}, tienes {edad} años.')
