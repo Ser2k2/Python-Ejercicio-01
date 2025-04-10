31a01db7cdbd0bcb3a917b242a424196a2ee964e
Ser2k2 <bell20002k@gmail.com>
2025-04-10 11:03:04 -0600

    New Files Created

---
+++ ejercicio056.py
@@ -0,0 +1,35 @@
+'''
+Desarrolla un programa que informe la puntuación de un estudiante de acuerdo con sus respuestas. 
+Debe pedir la respuesta del estudiante para cada pregunta y verificar si la respuesta coincide con el resultado. 
+Cada pregunta vale un punto y hay opciones A, B, C o D.
+
+Resultado del examen:
+01 - D
+02 - A
+03 - C
+04 - B
+05 - A
+06 - D
+07 - C
+08 - C
+09 - A
+10 - B
+'''
+
+# Inicializamos los datos
+respuestas = []  # Lista para almacenar las respuestas
+# Lista de respuestas correctas
+gabarito = ['D', 'A', 'C', 'B', 'A', 'D', 'C', 'C', 'A', 'B']
+nota = 0  # Acumulará la nota total
+
+# Recopilamos las respuestas del estudiante
+for i in range(0, 10):
+  respuestas.append(input(f'Introduzca la respuesta a la pregunta {i + 1}: ').upper())
+
+# Verificamos si las respuestas coinciden y las sumamos a la nota
+for i in range(0, 10):
+  if respuestas[i] == gabarito[i]:
+    nota += 1
+
+# Mostrando la nota final
+print(f'Nota final: {nota}')
